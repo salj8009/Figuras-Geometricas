1b3a65aa33561ca5be0b93081a83468751653331
JorgeSL <salj8009@gmail.com>
2022-06-05 14:35:10 -0500

    Creación de las funciones para realizar los calculos

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
-alert('saludos');
+console.group("Cuadrados");
+
+function perimetroCuadrado(lado) {
+    return lado * 4;
+}
+
+function areaCuadrado(lado) {
+    return lado * lado;
+}
+console.groupEnd();
+
+console.group("Triangulos");
+
+function perimetroTriangulo(lado1 , lado2 , base) {
+    return lado1 + lado2 + base;
+}
+
+function areaTriangulo(base , altura) {
+    return (base * altura) / 2;
+}
+
+console.groupEnd();
+
+console.group("Circulos");
+
+function diametroCirculo(radio) {
+    return radio * 2;
+}
+
+const PI = Math.PI;
+
+function perimetroCirculo(radio) {
+    const diametro = diametroCirculo(radio);
+    return diametro * PI;
+}
+
+function areaCirculo(radio) {
+    return (radio * radio) * PI;
+}
+
+console.groupEnd();
